c3c61f5dcb36a9489cc9b9be51a62b85cbfd8cfb
Manon Brousse <pandillamanolito@gmail.com>
2021-11-17 19:53:38 +0100

    dark mode

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ function init () {
 }
 
 init();
+
+const sunBtn = document.getElementById('sunBtn');
+const moon = document.getElementById('moon');
+
+document.querySelector(['[data-switch-dark]']).addEventListener('click',
+  function() {
+    document.body.classList.toggle('dark');
+});
