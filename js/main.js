8c373b3a90a2f880b0272baa6e779141b8207f03
Manon Brousse <pandillamanolito@gmail.com>
2021-11-17 19:54:51 +0100

    fix theme

--- js/main.js
+++ js/main.js
@@ -8,9 +8,6 @@ function init () {
 
 init();
 
-const sunBtn = document.getElementById('sunBtn');
-const moon = document.getElementById('moon');
-
 document.querySelector(['[data-switch-dark]']).addEventListener('click',
   function() {
     document.body.classList.toggle('dark');
