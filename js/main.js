2aad18bff9d6359a17fdf00a3dc566f75b328b82
Manon Brousse <pandillamanolito@gmail.com>
2021-11-18 21:21:08 +0100

    toggle details

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
 import '../scss/main.scss'
 
-function init () {
+function Main(){
 
-  const rootContainer = document.getElementById('root-container');
+  const moonBtn = document.getElementById('moonBtn');
+  moonBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+  });
+
+  const detailsContainer = document.getElementById('details');
+  const jobs = Array.from(document.getElementsByClassName('job'));
+  const details = Array.from(detailsContainer.children);
+
+  let previousIndex = 0;
+  details[previousIndex].style.display = 'flex';
+  jobs[previousIndex].classList.toggle('selected');
+
+  jobs.forEach((job, index) => job.addEventListener('click', () => {
+
+    const detail = details[index];
+
+    if(index === previousIndex) {
+
+      return false;
+
+    } else {
+
+      details[previousIndex].style.display = 'none';
+      jobs[previousIndex].classList.toggle('selected');
+
+      detail.style.display = 'flex';
+      job.classList.toggle('selected');
+      previousIndex = index;
+
+    }
+
+  }));
 
 }
 
-init();
-
-document.querySelector(['[data-switch-dark]']).addEventListener('click',
-  function() {
-    document.body.classList.toggle('dark');
-});
+document.addEventListener('DOMContentLoaded', Main);
