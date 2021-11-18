ea07b5c04c459f7f47cced2cd3f48a015c18fa7e
ninja-fire <44232936+ninja-fire@users.noreply.github.com>
2021-11-19 03:39:51 +0700

    Style (#5)
    * typo
    
    * button
    
    * side-menu
    
    * main container
    
    * style responsive
    
    * color theme
    
    * style fix
    
    * responsive
    
    * alt and fixes
    
    * details content
    
    * save
    
    * dark mode
    
    * fix theme
    
    * toggle details

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
 import '../scss/main.scss'
 
-function init () {
+function Main(){
 
-  const rootContainer = document.getElementById('root-container');
-  // new GameState(rootContainer);
+  const moonBtn = document.getElementById('moonBtn');
+  moonBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+  });
+
+  const detailsContainer = document.getElementById('details');
+  const jobs = Array.from(document.getElementsByClassName('job'));
+  const details = Array.from(detailsContainer.children);
+
+  let previousIndex = 0;
+  details[previousIndex].style.display = 'flex';
+  jobs[previousIndex].classList.toggle('selected');
+
+  jobs.forEach((job, index) => job.addEventListener('click', () => {
+
+    const detail = details[index];
+
+    if(index === previousIndex) {
+
+      return false;
+
+    } else {
+
+      details[previousIndex].style.display = 'none';
+      jobs[previousIndex].classList.toggle('selected');
+
+      detail.style.display = 'flex';
+      job.classList.toggle('selected');
+      previousIndex = index;
+
+    }
+
+  }));
+
 }
 
-init();
+document.addEventListener('DOMContentLoaded', Main);
